72bc6fd129796fc492441a8b97fe51e1b80cf86d
Corentin Chary <c.chary@criteo.com>
2018-04-23 20:03:46 +0200

    Update version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 setuptools.setup(
     name='gourde',
-    version='0.0.3',
+    version='0.1.0',
     include_package_data=True,
     install_requires=_INSTALL_REQUIRES,
     dependency_links=_DEPENDENCY_LINKS,
