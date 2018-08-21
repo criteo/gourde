6aef16ba68f6603caf39c70fd0eab769007e1a4e
Corentin Chary <c.chary@criteo.com>
2018-08-21 13:55:37 +0200

    Bump to 0.1.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 setuptools.setup(
     name='gourde',
-    version='0.1.5',
+    version='0.1.6',
     include_package_data=True,
     install_requires=_INSTALL_REQUIRES,
     dependency_links=_DEPENDENCY_LINKS,
