aa4ee3ccfe05cb4c3cc181d11c8af8bbc3929834
Alban Dericbourg <a.dericbourg@criteo.com>
2018-12-28 15:07:40 +0100

    Allow to disable logging configuration
    This is meant to give full control to the application to configure it
    
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

--- gourde/gourde_test.py
+++ gourde/gourde_test.py
@@ -1,8 +1,11 @@
 """Gourde unit tests."""
 
 from __future__ import absolute_import
+
 import unittest
+
 import flask
+import mockito
 import prometheus_client
 
 import gourde
@@ -31,6 +34,31 @@ class GourdeTest(unittest.TestCase):
         g = gourde.Gourde(app, registry=self.registry)
         testutils.setup(g)
         self.assertIsNotNone(g)
+
+    def test_disable_embedded_logging_should_prevent_logging_from_being_configured(self):
+        logger = mockito.mock()
+        app = flask.Flask(__name__)
+        app.logger = logger
+
+        g = gourde.Gourde(app, registry=self.registry)
+
+        testutils.setup(g, ["--disable-embedded-logging"])
+
+        mockito.verify(logger, times=0).addHandler(mockito.any())
+        mockito.verify(logger, times=0).setLevel(mockito.any())
+
+    def test_logging_should_be_enabled_by_default(self):
+        logger = mockito.mock()
+        app = flask.Flask(__name__)
+        app.logger = logger
+
+        g = gourde.Gourde(app, registry=self.registry)
+
+        testutils.setup(g, [])
+
+        mockito.verify(logger, times=1).addHandler(mockito.any())
+        mockito.verify(logger, times=1).setLevel(mockito.any())
+        mockito.verify(logger, times=1).info("Logging initialized.")
 
 
 # TODO:
